f299f452492a60add10fa3156f4b4d4caa7a4e98
Daniel Simmons <dan@dans.im>
2013-08-02 10:23:59 +0100

    Clean up old stuff from manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,3 @@
-import subprocess
-from os.path import join, abspath, dirname
 from app import create_app
 from flask import current_app
 from flask.ext.script import Shell, Manager, Server
@@ -11,9 +9,7 @@ def _make_shell_context():
     """
     Shell context: import helper objects here.
     """
-    from app.db import db
-    from app.ldapconn import ldap
-    return dict(app=current_app, db=db, ldap=ldap)
+    return dict(app=current_app)
 
 
 manager.add_option('--flask-config', dest='config', help='Specify Flask config file', required=False)
